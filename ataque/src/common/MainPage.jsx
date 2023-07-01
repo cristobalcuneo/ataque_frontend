0dae4b48496731332fb7cb9ce48089533241b3a9
cristobalcuneo <cristobal.cuneo@uc.cl>
2023-07-01 19:47:52 -0400

    cambio carpeta assets a public

--- ataque/src/common/MainPage.jsx
+++ ataque/src/common/MainPage.jsx
@@ -15,12 +15,12 @@ function MainPage() {
                 <div className='gamecards-container'>
                     <p>Selecciona uno de los modos de juego</p>
                     <div className='gamecards-container-row'>
-                        <MainPageGameCard imgSrc={"./src/assets/img/w1.jpeg"} name={"ATAQUE"} description={""}/>
-                        <MainPageGameCard imgSrc={"./src/assets/img/w2.jpg"} name={"DEFENSA"} description={""}/>
+                        <MainPageGameCard imgSrc={"./assets/img/w1.jpeg"} name={"ATAQUE"} description={""}/>
+                        <MainPageGameCard imgSrc={"./assets/img/w2.jpg"} name={"DEFENSA"} description={""}/>
                     </div>
                     <div className='gamecards-container-row'>
-                        <MainPageGameCard imgSrc={"./src/assets/img/w3.jpg"} name={"AEREO"} description={""}/>
-                        <MainPageGameCard imgSrc={"./src/assets/img/w4.jpg"} name={"TANQUES"} description={""}/>
+                        <MainPageGameCard imgSrc={"./assets/img/w3.jpg"} name={"AEREO"} description={""}/>
+                        <MainPageGameCard imgSrc={"./assets/img/w4.jpg"} name={"TANQUES"} description={""}/>
                     </div>
                 </div>
             </div>
